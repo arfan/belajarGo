fd1f4531e4d271cbf6c0d7d28de70520f1690017
Abdul Arfan <abdul.arfan@gmail.com>
2017-11-19 19:31:50 +0700

    adding loop

---
+++ 02_numeral-system/04_loop/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i:= 0;i<100;i++ {
+		fmt.Println("i=", i)
+	}
+	for i := 1000000; i < 1000100; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	}
+}
